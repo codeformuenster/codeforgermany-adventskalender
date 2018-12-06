e56b89cab21f85213e867acfe1652882e87d8da6
Stefan Grotz <stefan.grotz@1und1.de>
2018-12-06 10:54:28 +0100

    Tür 25: Dein Projekt

--- js/index.js
+++ js/index.js
@@ -121,7 +121,11 @@ $( document ).ready(function() {
     "Jede Person hat das Recht auf Informationen. FragDenStaat hilft Ihnen, Ihr Recht wahrzunehmen. Fragen Sie über diese gemeinnützige Plattform Behörden in Deutschland nach Informationen und Dokumenten! ",
     "https://fragdenstaat.de/"
     ], 
-     "25"];
+    [ "X-200",
+    "Dein Projekt",
+    "Jede Woche treffen sich in ganz Deutschland Teams zu ihren Projekten für Code for Germany. Sie arbeiten gemeinsam an nützlichen Anwendungen und Visualisierungen rund um offene Daten und digitale Werkzeuge für Bürgerinnen. Diese lokalen Gruppen nennen wir Labs. Wenn Du eine Idee für ein Projekt hast dann schau auf codefor.de wo es in deiner Nähe ein Lab gib.",
+    "https://codefor.de/"
+    ]];
  console.log("test");
   var message = "";
   var date = new Date();
